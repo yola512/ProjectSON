784aa74c6719a6379a2afd0cc98acc97eb29a2b9
Yola <jolamajcher12@gmail.com>
2024-12-22 23:42:39 +0100

    Edited entry_point

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
 
 version = os.getenv(
     "VERSION", "0.0.0"
-)  # Fallback to '0.0.0'version = os.getenv('PACKAGE_VERSION', '0.0.0')  # Fallback to '0.0.0'
+)  # Fallback to '0.0.0' version = os.getenv('PACKAGE_VERSION', '0.0.0')  # Fallback to '0.0.0'
 
 
 
@@ -17,13 +17,13 @@ setup(
     author="Jolanta Majcher",
     author_email="jolamajcher12@gmail.com",
     description="Student attendance management",
-    long_description=open("README.md").read(),
+    long_description = open("README.md").read() if os.path.exists("README.md") else "",
     long_description_content_type="text/markdown",
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
     entry_points={
-        "console_scripts": ["projectSON="], #idk :c
+        "console_scripts": ["projectSON=src.modules.main:main"], #main() in main.py is an entry point :)
     },
     classifiers=[
         "Programming Language :: Python :: 3"
